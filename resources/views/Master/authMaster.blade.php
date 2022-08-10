a6c9e251e4ae9e296b206a169a81bdd79c02ad8b
Huwnga <hungbungbu2003@gmail.com>
2022-08-10 17:44:30 +0700

    auth

--- resources/views/Master/authMaster.blade.php
+++ resources/views/Master/authMaster.blade.php
@@ -63,7 +63,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="#">Durable Furnitures</a>
+        <a class="navbar-brand" href="#">Thrift Fashion</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
