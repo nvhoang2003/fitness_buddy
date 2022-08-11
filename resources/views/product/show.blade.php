d9b0bef31abad9b82ca73ece48d3f72518a8e6ce
anhlan09 <lethianhlan2003@gmail.com>
2022-08-11 09:51:41 +0700

    createproduct

---
+++ resources/views/product/show.blade.php
@@ -0,0 +1,9 @@
+@extends('Master.adminMaster')
+
+@section('main')
+    <div class="container">
+        <h1 class="display-4">Product Details</h1>
+        @include('Furniture_shop.products.productDetails')
+        <a type="button" href="{{route('product.index.blade.php')}}" class="btn btn-info">Back to Index</a>
+    </div>
+@endsection
