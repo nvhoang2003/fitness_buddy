ba9c4870c567d7cd608c1554e818591c59630af4
duong141001 <107864382+duong141001@users.noreply.github.com>
2022-08-11 14:31:11 +0700

    homepage add master

--- resources/views/client/homepage.blade.php
+++ resources/views/client/homepage.blade.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Boutique | Ecommerce bootstrap template</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- gLightbox gallery-->
-    <link rel="stylesheet" href={{asset("vendor/glightbox/css/glightbox.min.css")}}>
-    <!-- Range slider-->
-    <link rel="stylesheet" href={{asset("vendor/nouislider/nouislider.min.css")}}>
-    <!-- Choices CSS-->
-    <link rel="stylesheet" href={{asset("vendor/choices.js/public/assets/styles/choices.min.css")}}>
-    <!-- Swiper slider-->
-    <link rel="stylesheet" href={{asset("vendor/swiper/swiper-bundle.min.css"}}>
-    <!-- Google fonts-->
-    <link rel="stylesheet" href={{asset("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap")}}>
-    <link rel="stylesheet" href={{asset("https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap")}}>
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href={{asset("public/css/style.default.css")}} id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href={{asset("css/custom.css")}}>
-    <!-- Favicon-->
-    <link rel="shortcut icon" href={{asset("img/favicon.png")}}>
-</head>
-<body>
+
+@extends('Master.clientMaster')
+
+@section('main')
 <div class="page-holder">
     <!-- navbar-->
     <header class="header bg-white">
@@ -454,5 +430,4 @@
     <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </div>
-</body>
-</html>
+@endsection
