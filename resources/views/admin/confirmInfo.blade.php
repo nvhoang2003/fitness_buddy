fc1bd4bd5c1e2bf3333eb14a24f59f131525ea48
KhacDuongg <khacduongdo12@gmail.com>
2022-08-12 12:43:54 +0700

    commit

---
+++ resources/views/admin/confirmInfo.blade.php
@@ -0,0 +1,58 @@
+@extends('Master.adminMaster')
+
+@section('main')
+    <div class="container col-md-8">
+            <h1 class="display-6">Account Info</h1>
+            @include('partials.sessionMessage')
+            <form action="{{route('admin.adminUpdateInfo',['username' => old('username') ?? $user->username])}}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="username" class="account">Username: {{$user->username}}</label>
+                    <input type="hidden" class="form-control" id="username" name="username" value="{{old('username') ?? $user->username}}">
+                </div>
+                <div class="form-group">
+                    <label for="contact" class="account">Contact</label>
+                    <input type="text" class="form-control" id="contact" name="contact" value="{{old('contact') ?? $user->contact}}">
+                    @if($errors->has('contact'))
+                        @foreach($errors->get('contact') as $e)
+                            <span class="danger help-box">
+                            <i class="bi bi-x"></i>
+                            {{$e}}
+                        </span>
+                        @endforeach
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="email" class="account">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{old('email') ?? $user->email}}">
+                    @if($errors->has('email'))
+                        @foreach($errors->get('email') as $e)
+                            <span class="danger help-box">
+                            <i class="bi bi-x"></i>
+                            {{$e}}
+                        </span>
+                        @endforeach
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="password" class="account">Password for confirm</label>
+                    <input type="password" class="form-control" id="password" name="password" >
+                    @if($errors->has('password'))
+                        @foreach($errors->get('password') as $e)
+                            <span class="danger help-box">
+                            <i class="bi bi-x"></i>
+                            {{$e}}
+                        </span>
+                        @endforeach
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-dark">Save</button>
+                <a type="button" href="{{route('admin.adminChangePassword', ['username' => $user->username])}}"
+                   class="btn btn-info">
+                    Change Password
+                    <i class="bi bi-box-arrow-right"></i>
+                </a>
+            </form>
+        </div>
+@endsection
+
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,33 @@
+@extends('Master.adminMaster')
+
+@section('main')
+    <div class="container">
+        <h2 id="icons" class="display-4 mb-0 col-md-8">Customer Index</h2>
+        @include('partials.sessionmessage')
+        <table class="table table-hover">
+            <thead class="thead" style="background-color: #acb2c7">
+            <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Fullname</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($customers as $c)
+                <tr>
+                    <td>{{$c->username}}</td>
+                    <td>{{$c->fullname}}</td>
+                    <td>{{$c->gender}}</td>
+                    <td>{{$c->phonenumber}}</td>
+                    <td>{{$c->email}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+
+    </div>
+@endsection
+
