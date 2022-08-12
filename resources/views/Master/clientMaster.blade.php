a89a98adf529b291455fcf14ca11a71b533cd6f5
tuanbui03 <tuanbui.10103@gmail.com>
2022-08-12 12:59:47 +0700

    client

--- resources/views/Master/clientMaster.blade.php
+++ resources/views/Master/clientMaster.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     <!-- Boostrap 5.0.2  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
@@ -38,6 +39,7 @@
     <link rel="stylesheet" href={{asset("css/custom.css")}}>
     <!-- Favicon-->
     <link rel="shortcut icon" href={{asset("img/favicon.png")}}>
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
 </head>
 <body>
 
